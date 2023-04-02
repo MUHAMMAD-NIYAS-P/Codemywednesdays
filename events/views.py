b44273d10b06e4d41c2b302f049ee3386226c914
MUHAMMAD-NIYAS-P <mohdniyasp98@gmail.com>
2023-04-03 01:42:54 +0530

    This is second commit of djwed previous where 2 and this is 3rd video commit 2

--- events/views.py
+++ events/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Create your views here.
 
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     name = "Jhon"
 
     month = month.capitalize()
