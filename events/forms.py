2ecf702d0f2491bcc832d59d03fd210de45428bf
MUHAMMAD-NIYAS-P <mohdniyasp98@gmail.com>
2023-04-03 11:08:45 +0530

    This is sixth commit v7(how to add database form to a website)

---
+++ events/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Venue
+
+class VenueForm(forms.ModelForm):
+
+    class Meta:
+        model = Venue
+        fields = '__all__'
+
+        labels = {
+            'name': '',
+            'address': '',
+            'zip_code': '',
+            'phone': '',
+            'web': '',
+            'email_address': '',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Venue Name'}),
+            'address': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address'}),
+            'zip_code': forms.TextInput(attrs={'class':'form-control', 'placeholder':'zip code'}),
+            'phone': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}),
+            'web': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Web Address'}),
+            'email_address': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}),
+        }
